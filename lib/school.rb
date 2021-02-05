1a52cb7c8008df91761b0aa0d2458739cce2d411
Blake Runyon <blakerunyon09@gmail.com>
2021-02-04 17:10:30 -0700

    commit tree 8cc68ceeebc9506e86a3b074aef80dde4db28a76

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
-# code here!
+class School
+    attr_reader :roster
+
+    def initialize name, roster = {}
+        @name = name
+        @roster = roster
+    end
+
+    def add_student name, age
+        roster[age] ? roster[age].push(name) : roster[age] = [name]
+    end
+
+    def grade age
+        roster[age]
+    end
+
+    def sort
+        roster.each { |k,v| roster[k] = v.sort }
+    end
+end
+
+school = School.new("Bayside High School")
+school.add_student("Zach Morris", 9)
+school.add_student("AC Slater", 9)
+school.add_student("Kelly Kapowski", 10)
+school.add_student("Screech", 11)
+
+pp school.sort
